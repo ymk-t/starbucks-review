6d992f050af2b8fd5098d34235d5145253baf74f
Yumeki <yumekui64@gmail.com>
2019-11-04 22:00:18 +0900

    map.js dotenv&input&return修正

--- functions/map.js
+++ functions/map.js
@@ -1,21 +1,26 @@
 const axios = require('axios')
+require('dotenv').config()
 
-exports.handler = async function (event, context) {
-  console.log(event, context)
-  const response = await axios.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json', {
-    method: 'get',
-    params: {
-      language: 'ja',
-      fields:
-        'formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types',
-      locationbias: 'point:35.003977299999995,135.7639905',
-      input: '京都',
-      inputtype: 'textquery',
-      key: process.env.GOOGLE_MAP_API
+exports.handler = async function(event, context) {
+  const response = await axios.get(
+    'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+    {
+      method: 'get',
+      params: {
+        language: 'ja',
+        fields:
+          'formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types',
+        locationbias: 'point:35.003977299999995,135.7639905',
+        input: event.queryStringParameters.input,
+        inputtype: 'textquery',
+        key: process.env.GOOGLE_MAP_API
+      }
     }
-  })
+  )
+  console.log(response.data.candidates[0])
+
   return {
     statusCode: 200,
-    body: JSON.stringify(response)
+    body: JSON.stringify(response.data.candidates[0])
   }
 }
