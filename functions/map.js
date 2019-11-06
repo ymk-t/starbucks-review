db32b5afb398ac51c27f86808131dc6bd38dd6d5
Yumeki <yumekui64@gmail.com>
2019-11-06 22:00:23 +0900

    modify map.js

--- functions/map.js
+++ functions/map.js
@@ -3,18 +3,21 @@ const axios = require('axios')
 require('dotenv').config()
 
 exports.handler = async function(event, context) {
-  const response = await axios.get('https://stareco.netlify.com/.netlify/functions/map', {
-    method: 'get',
-    params: {
-      language: 'ja',
-      fields:
-        'formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types',
-      locationbias: 'point:35.003977299999995,135.7639905',
-      input: event.queryStringParameters.input + ' スターバックス',
-      inputtype: 'textquery',
-      key: process.env.GOOGLE_MAP_API
+  const response = await axios.get(
+    'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+    {
+      method: 'get',
+      params: {
+        language: 'ja',
+        fields:
+          'formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types',
+        locationbias: 'point:35.003977299999995,135.7639905',
+        input: event.queryStringParameters.input + ' スターバックス',
+        inputtype: 'textquery',
+        key: process.env.GOOGLE_MAP_API
+      }
     }
-  })
+  )
 
   return {
     statusCode: 200,
