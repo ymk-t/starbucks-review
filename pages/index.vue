8055af0077f06ea71c090bd3b9b647715d01e94e
Yumeki <yumekui64@gmail.com>
2019-11-06 21:36:26 +0900

    Change searchResult

--- pages/index.vue
+++ pages/index.vue
@@ -55,18 +55,20 @@ export default {
   },
   methods: {
     async searchResult() {
-      const response = await this.$axios.$get('/maps', {
-        method: 'get',
-        params: {
-          language: 'ja',
-          fields:
-            'formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types',
-          locationbias: 'point:35.003977299999995,135.7639905',
-          input: this.searchQuery,
-          inputtype: 'textquery',
-          key: process.env.GOOGLE_MAP_API
+      const response = await this.$axios.$get(
+        'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+        {
+          method: 'get',
+          params: {
+            language: 'ja',
+            fields:
+              'formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types',
+            input: this.searchQuery,
+            inputtype: 'textquery',
+            key: process.env.GOOGLE_MAP_API
+          }
         }
-      })
+      )
       this.map = response.candidates[0]
     }
   }
