fd2d6ff0ba00ef84973db2232474e615e8518b28
Yumeki <yumekui64@gmail.com>
2019-11-08 10:17:18 +0900

    Change param 'input'

--- functions/map.js
+++ functions/map.js
@@ -12,7 +12,7 @@ exports.handler = async function(event, context) {
         fields:
           'formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types',
         locationbias: 'point:35.003977299999995,135.7639905',
-        input: event.queryStringParameters.input + ' スターバックス',
+        input: event.queryStringParameters.input + '+スターバックス',
         inputtype: 'textquery',
         key: process.env.GOOGLE_MAP_API
       }
