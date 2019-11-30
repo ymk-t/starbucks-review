72d4f34a48887fdaa8e89b9ab5b6d656bb3b42f0
Yumeki <yumekui64@gmail.com>
2019-11-30 12:31:38 +0900

    add style sheet for respponsibe design

--- components/TopHeaderWrap.vue
+++ components/TopHeaderWrap.vue
@@ -27,95 +27,191 @@ export default {
 </script>
 
 <style>
-.Header-wrapper {
-  height: 65px;
-  width: 100%;
-  position: fixed;
-  background-color: lightgrey;
-  opacity: 1;
-  top: 0;
-  z-index: 20;
+@media screen and (min-width: 1000px) {
+  .Header-wrapper {
+    height: 65px;
+    width: 100%;
+    position: fixed;
+    background-color: lightgrey;
+    opacity: 1;
+    top: 0;
+    z-index: 20;
+  }
+
+  .Header-logo {
+    width: 200px;
+    opacity: 1;
+    float: left;
+  }
+
+  .search_container {
+    box-sizing: border-box;
+    position: relative;
+    border: 1px solid black;
+    display: block;
+    margin-top: 25px;
+    margin-right: 20px;
+    border-radius: 20px;
+    width: 250px;
+    overflow: hidden;
+    float: right;
+  }
+  .search_container input[type='text'] {
+    border: none;
+    padding-left: 10px;
+  }
+  .search_container input[type='text']:focus {
+    outline: 0;
+  }
+  .search_container input[type='submit'] {
+    box-sizing: inherit;
+    cursor: pointer;
+    width: 40px;
+    font-family: FontAwesome;
+    font-size: 16px;
+    border: none;
+    background: #3879d9;
+    color: white;
+    position: absolute;
+    right: 0;
+    top: 0;
+    outline: none;
+  }
+
+  .Header-button {
+    max-width: 180px;
+    text-align: center;
+    font-size: 16px;
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+    padding: 10px 10px;
+    border-radius: 4px;
+    margin: 10px;
+    float: right;
+  }
+
+  .Header-button:active {
+    transform: translateY(4px);
+    border-bottom: none;
+  }
+
+  .register {
+    width: 95px;
+    background-color: green;
+    border-bottom: 2px solid darkgreen;
+  }
+
+  .login {
+    color: green;
+    font-size: 18px;
+    width: 95px;
+    background-color: rgb(240, 240, 240);
+    border-bottom: 2px solid darkgray;
+  }
+
+  .register:hover {
+    background-color: rgba(60, 210, 60);
+    border-bottom: 2px solid green;
+  }
+
+  .login:hover {
+    background-color: white;
+    border-bottom: 2px solid darkgray;
+  }
 }
 
-.Header-logo {
-  width: 200px;
-  opacity: 1;
-  float: left;
-}
+@media screen and (max-width: 999px) {
+  .Header-wrapper {
+    height: 40px;
+    width: 100%;
+    position: fixed;
+    background-color: lightgrey;
+    opacity: 1;
+    top: 0;
+    z-index: 12;
+  }
 
-.search_container {
-  box-sizing: border-box;
-  position: relative;
-  border: 1px solid black;
-  display: block;
-  margin-top: 25px;
-  margin-right: 20px;
-  border-radius: 20px;
-  width: 250px;
-  overflow: hidden;
-  float: right;
-}
-.search_container input[type='text'] {
-  border: none;
-  padding-left: 10px;
-}
-.search_container input[type='text']:focus {
-  outline: 0;
-}
-.search_container input[type='submit'] {
-  box-sizing: inherit;
-  cursor: pointer;
-  width: 40px;
-  font-family: FontAwesome;
-  font-size: 16px;
-  border: none;
-  background: #3879d9;
-  color: white;
-  position: absolute;
-  right: 0;
-  top: 0;
-  outline: none;
-}
+  .Header-logo {
+    width: 200px;
+    opacity: 1;
+    float: left;
+  }
 
-.Header-button {
-  max-width: 180px;
-  text-align: center;
-  font-size: 16px;
-  color: #fff;
-  text-decoration: none;
-  font-weight: bold;
-  padding: 10px 10px;
-  border-radius: 4px;
-  margin: 10px;
-  float: right;
-}
+  .search_container {
+    box-sizing: border-box;
+    position: relative;
+    border: 1px solid black;
+    display: block;
+    margin-top: 16px;
+    margin-right: 12px;
+    border-radius: 12px;
+    width: 160px;
+    overflow: hidden;
+    float: right;
+  }
+  .search_container input[type='text'] {
+    border: none;
+    padding-left: 6px;
+  }
+  .search_container input[type='text']:focus {
+    outline: 0;
+  }
+  .search_container input[type='submit'] {
+    box-sizing: inherit;
+    cursor: pointer;
+    width: 27px;
+    font-family: FontAwesome;
+    font-size: 11px;
+    border: none;
+    background: #3879d9;
+    color: white;
+    position: absolute;
+    right: 0;
+    top: 0;
+    outline: none;
+  }
 
-.Header-button:active {
-  transform: translateY(4px);
-  border-bottom: none;
-}
+  .Header-button {
+    max-width: 120px;
+    text-align: center;
+    font-size: 11px;
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+    padding: 7px 7px;
+    border-radius: 2px;
+    margin: 7px;
+    float: right;
+  }
 
-.register {
-  width: 95px;
-  background-color: green;
-  border-bottom: 2px solid darkgreen;
-}
+  .Header-button:active {
+    transform: translateY(2px);
+    border-bottom: none;
+  }
 
-.login {
-  color: green;
-  font-size: 18px;
-  width: 95px;
-  background-color: rgb(240, 240, 240);
-  border-bottom: 2px solid darkgray;
-}
+  .register {
+    width: 60px;
+    background-color: green;
+    border-bottom: 2px solid darkgreen;
+  }
 
-.register:hover {
-  background-color: rgba(60, 210, 60);
-  border-bottom: 2px solid green;
-}
+  .login {
+    color: green;
+    font-size: 12px;
+    width: 60px;
+    background-color: rgb(240, 240, 240);
+    border-bottom: 2px solid darkgray;
+  }
 
-.login:hover {
-  background-color: white;
-  border-bottom: 2px solid darkgray;
+  .register:hover {
+    background-color: rgba(60, 210, 60);
+    border-bottom: 2px solid green;
+  }
+
+  .login:hover {
+    background-color: white;
+    border-bottom: 2px solid darkgray;
+  }
 }
 </style>
